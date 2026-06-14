f9f179b40c58644874d1e2e79c2bcbbaabcef80c
mongzera <40034319+mongzera@users.noreply.github.com>
2026-06-14 13:11:09 +0800

    bug: Fixed memory allocation inconsistency

--- memory/memory.h
+++ memory/memory.h
@@ -1,6 +1,7 @@
 #ifndef H_MEMORY
 #define H_MEMORY
 
+#include <stdio.h>
 #define mem_is_out_of_bounds(addr, start, size) ((addr) < (start) || (addr) >= ((start) + (size)))
 
 #include "../core/kdvm.h"
@@ -18,20 +19,23 @@ uint32_t        malloc_heap(VM* vm, uint32_t size);
 void            free_heap(VM* vm, uint32_t addr);
 
 static inline uint32_t store_stack(VM* vm, uint32_t fp, uint32_t offset, PrimitiveValue value) {
-    if(!mem_is_out_of_bounds(fp + offset, STACK_RAM_START, STACK_RAM_SIZE)) {
-        vm_error("SegFault: Stack out of bounds!");
+    uint32_t address = STACK_RAM_START + fp + offset;
+
+    if(mem_is_out_of_bounds(address, STACK_RAM_START, STACK_RAM_SIZE)) {
+        vm_error("SegFault: Stack store out of bounds!");
         exit(-1);
     }
-    vm->ram[STACK_RAM_START + fp + offset] = value;
+    vm->ram[address] = value;
     return 0;
 }
 
 static inline PrimitiveValue load_stack(VM* vm, uint32_t fp, uint32_t offset) {
-    if(!mem_is_out_of_bounds(fp + offset, STACK_RAM_START, STACK_RAM_SIZE)) {
-        vm_error("SegFault: Stack out of bounds!");
+    uint32_t address = STACK_RAM_START + fp + offset;
+    if(mem_is_out_of_bounds(address, STACK_RAM_START, STACK_RAM_SIZE)) {
+        vm_error("SegFault: Stack load out of bounds!");
         exit(-1);
     }
-    return vm->ram[STACK_RAM_START + fp + offset];
+    return vm->ram[address];
 }
 
 #endif
