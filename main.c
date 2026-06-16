25532141fc72754923d8c5da1b0551dc7a005f40
mongzera <gmtevan@gmail.com>
2026-06-16 08:23:45 +0800

    feat: created commands

--- main.c
+++ main.c
@@ -3,20 +3,29 @@
 #include "assembler/assember.h"
 #include <time.h>
 
-int main(int argc, char* argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s <program.kdm>\n", argv[0]);
-        return 1;
-    }
+typedef enum Commands {
+    COMMAND_COMPILE = 0x0,
+    COMMAND_RUN
+};
 
+void print_unknown_command(int argc, char* argv[]){
+    printf("Unknown Command: %s\n", argv[1]);
+    printf("Idk ts, man, type properly\n");
+}
+
+void print_help(int argc, char* argv[]){
+    printf("Help!!!\n");
+    printf("Usage: %s -r, --run <program.kdo> to run objects.\n", argv[0]);
+    printf("Usage: %s -c, --help <program.kdm> to compile to objects.\n", argv[0]);
+}
+
+void command_run_vm(char* filename){
     // Initialize a Single VM Instance for now
     VM my_machine;
     vm_init(&my_machine);
 
     // Load the program into this specific instance
-    if (load_kdm_file(argv[1], &my_machine) != 0) {
-        return 1;
-    }
+    if (load_kdm_file(filename, &my_machine) != 0) return;
 
     struct timespec start, end;
 
@@ -31,5 +40,22 @@ int main(int argc, char* argv[]) {
     long long elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
 
     printf("Execution time: %lld nanoseconds\n", elapsed_ns);
+}
+
+int command = -1;
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        print_help(argc, argv);
+        return 1;
+    }
+
+    if(strcmp(argv[1], "-r") == 0 || strcmp(argv[1], "--run") == 0) command = COMMAND_RUN;
+    else if(strcmp(argv[1], "-c") == 0 || strcmp(argv[1], "--compile") == 0) command = COMMAND_RUN;
+    else if(strcmp(argv[1], "-h") == 0 || strcmp(argv[1], "--help") == 0) print_help(argc, argv);
+    else print_unknown_command(argc, argv);
+
+    if(command == -1) return 1;
+    if(command == COMMAND_RUN) command_run_vm(argv[2]);
+
     return 0;
 }
